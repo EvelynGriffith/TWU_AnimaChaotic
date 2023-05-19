65e4d7c9e58d6f60c066c6b3377cf27f5ff68493
EvelynGriffith <griffith01@allegheny.edu>
2023-05-19 12:24:06 -0700

    trying to get scenefilereader to work with the other files

--- src/SceneFileReader.py
+++ src/SceneFileReader.py
@@ -1,19 +1,27 @@
 
 import json
 from collections import namedtuple
+
+filename = "Story1.json"
+with open(filename, "w") as f:
+  while True:
+    user_input = f.write(input("Add your story text here: "))
+    #       f.write("\n")
+    #   except EOFError:
+    break
+
+# def get_inputs():
+# #user input text to animate
+#     text = input("please add the text you would like to animate: ")
+#     return(text)
+
+# out = {user_input}
+# # # generate the json file with the user text
+# filename = open(r'Story1.json','w')
+# with filename:
+#     json.dump(out, filename, indent=2)
+
 class SceneFileReader(object):
-
-
-    def get_inputs():
-    #user input text to animate
-        text = input("please add the text you would like to animate: ")
-        return(text)
-
-    out = {}
-    # generate the json file with the user text
-    with open('Story1.json','w') as filename:
-        json.dump(out, filename, indent=2)
-
     # attempting to read the filename and set it to filename
     def __init__(self,filename):
 
@@ -22,7 +30,7 @@ class SceneFileReader(object):
     # reading the file and attributing scene nodes
     def readFile(self):
 
-        with open(self.filename) as data_file:
+        with filename as data_file:
             data = json.load(data_file)
 
         scene_nodes = [namedtuple('SceneNode',node.keys())(*node.values()) for node in data["scene_nodes"]]
